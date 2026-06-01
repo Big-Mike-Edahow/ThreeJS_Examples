47d5b3299300d19404f045442278f050cf053e43
Big-Mike-Edahow <perigran_falcon@yahoo.com>
2026-05-31 20:37:56 -0600

    I'm getting better at this.

---
+++ TSL_Angular_Slicing/README.md
@@ -0,0 +1,1 @@
+# ThreeJS TSL Angular Slicing
---
+++ TSL_Angular_Slicing/static/js/main.js
@@ -0,0 +1,153 @@
+// main.js
+
+import * as THREE from 'three/webgpu';
+import { If, TWO_PI, atan, color, frontFacing } from 'three/tsl';
+import { output, positionLocal, Fn, uniform, vec4 } from 'three/tsl';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { UltraHDRLoader } from 'three/addons/loaders/UltraHDRLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { Inspector } from 'three/addons/inspector/Inspector.js';
+
+function main() {
+    // Scene.
+    const scene = new THREE.Scene();
+
+    // Camera.
+    const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 100);
+    camera.position.set(- 5, 5, 12);
+
+    // Lights.
+    const directionalLight = new THREE.DirectionalLight('#ffffff', 4);
+    directionalLight.position.set(6.25, 3, 4);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.set(2048, 2048);
+    directionalLight.shadow.camera.near = 0.1;
+    directionalLight.shadow.camera.far = 30;
+    directionalLight.shadow.camera.top = 8;
+    directionalLight.shadow.camera.right = 8;
+    directionalLight.shadow.camera.bottom = - 8;
+    directionalLight.shadow.camera.left = - 8;
+    directionalLight.shadow.normalBias = 0.05;
+    scene.add(directionalLight);
+
+    // Renderer.
+    const renderer = new THREE.WebGPURenderer({ antialias: true });
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 1;
+    renderer.shadowMap.enabled = true;
+    renderer.inspector = new Inspector();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    const container = document.querySelector('#threejs-container');
+    container.appendChild(renderer.domElement);
+
+    // Orbit controls.
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.minDistance = 0.1;
+    controls.maxDistance = 50;
+
+    // TSL functions
+    const inAngle = Fn(([position, angleStart, angleArc]) => {
+        const angle = atan(position.y, position.x).sub(angleStart).mod(TWO_PI).toVar();
+        return angle.greaterThan(0).and(angle.lessThan(angleArc));
+    });
+
+    // Materials
+    const defaultMaterial = new THREE.MeshPhysicalNodeMaterial({
+        metalness: 0.5,
+        roughness: 0.25,
+        envMapIntensity: 0.5,
+        color: '#858080'
+    });
+    const slicedMaterial = new THREE.MeshPhysicalNodeMaterial({
+        metalness: 0.5,
+        roughness: 0.25,
+        envMapIntensity: 0.5,
+        color: '#858080',
+        side: THREE.DoubleSide
+    });
+
+    // Uniforms.
+    const sliceStart = uniform(1.75);
+    const sliceArc = uniform(1.25);
+    const sliceColor = uniform(color('#b62f58'));
+
+    // Mask.
+    const mask = inAngle(positionLocal.xy, sliceStart, sliceArc).not();
+    slicedMaterial.maskNode = mask;
+
+    // Output.
+    slicedMaterial.outputNode = Fn(() => {
+        // Backface color.
+        const finalOutput = output;
+        If(frontFacing.not(), () => {
+            finalOutput.assign(vec4(sliceColor, 1));
+        });
+        return finalOutput;
+    })();
+
+    // HDR Loader.
+    const hdrLoader = new UltraHDRLoader();
+    hdrLoader.load('static/textures/royal_esplanade_2k.hdr.jpg', (environmentMap) => {
+        environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+        scene.background = environmentMap;
+        scene.environment = environmentMap;
+    });
+
+    // Draco Loader.
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
+
+    // GLTF Loader.
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.setDRACOLoader(dracoLoader);
+    gltfLoader.load('static/models/gears.glb', (gltf) => {
+        const model = gltf.scene;
+        model.traverse((child) => {
+            if (child.isMesh) {
+                if (child.name === 'outerHull')
+                    child.material = slicedMaterial;
+                else
+                    child.material = defaultMaterial;
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        });
+        scene.add(model);
+    });
+
+    // Plane.
+    const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(10, 10, 10),
+        new THREE.MeshStandardMaterial({ color: '#aaaaaa' })
+    );
+    plane.receiveShadow = true;
+    plane.position.set(- 4, - 3, - 4);
+    plane.lookAt(new THREE.Vector3(0, 0, 0));
+    scene.add(plane);
+
+    // GUI.
+    const gui = renderer.inspector.createParameters('Parameters');
+    gui.add(sliceStart, 'value', - Math.PI, Math.PI, 0.001).name('sliceStart');
+    gui.add(sliceArc, 'value', 0, Math.PI * 2, 0.001).name('sliceArc');
+    gui.addColor({ color: sliceColor.value.getHexString(THREE.SRGBColorSpace) }, 'color').onChange(value => sliceColor.value.set(value));
+
+    // Window resize.
+    window.addEventListener('resize', onWindowResize);
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    // Main animation loop.
+    async function animate() {
+        controls.update();
+        renderer.render(scene, camera);
+    }
+}
+
+main();
